a868909b62b646454d6c587fb3a0e7cd43b9c30c
virgilio1974 <virgifp@yahoo.es>
2016-03-07 17:24:34 +0100

    namespace api-mentor api/v1

--- app/controllers/api/v1/conversations_controller.rb
+++ app/controllers/api/v1/conversations_controller.rb
@@ -1,4 +1,5 @@
-class ConversationsController < ApplicationController
+class Api::V1::ConversationsController < ApplicationController
+
   before_action :set_conversation, only: [:show, :update, :destroy]
 
   # GET /conversations
